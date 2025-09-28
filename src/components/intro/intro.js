edf737694e4153cce8f44a43766de7f7ada0333b
coin-123 <isobednlemchukwu@gmail.com>
2025-09-28 15:20:44 +0100

    fix: improve code formatting and structure in Intro component

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import './intro.css';
-import bg from '../../asset/obed.png';
-import hiremeimg from '../../asset/hire-me.png'
-import { Link } from 'react-scroll';
+import React from "react";
+import "./intro.css";
+import bg from "../../asset/obed.png";
+import hiremeimg from "../../asset/hire-me.png";
+import { Link } from "react-scroll";
 
 
 const Intro = () => {
   return (
-   <section id="intro">
-    <div className="introcontent">
-    <span className="hello">Hello,</span>
-    <span className="introtext">I'm <span className="introname">O'bed</span> <br /> Website Designer</span>
-    <p className="intropara">I am a skilled web designer with experience in creating <br/>visual appealing and user freindly websites</p>
-    <Link><button className="btn"> <img src={hiremeimg} alt=""  className="hiremeimg"/> Hire Me</button> </Link>
-    </div>
-    <img src={bg} alt="Profile" className="bg" />
-   </section>
-  )
-}
+    <section id="intro">
+      <div className="introcontent">
+        <span className="hello">Hello,</span>
+        <span className="introtext">
+          I'm <span className="introname">O'bed</span> <br /> Website Designer
+        </span>
+        <p className="intropara">
+          I am a skilled web designer with experience in creating <br />
+          visual appealing and user freindly websites
+        </p>
+        <Link
+           activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-50}
+          duration={500}
+        >
+          <button className="btn">
+            {" "}
+            <img src={hiremeimg} alt="" className="hiremeimg" /> Hire Me
+          </button>{" "}
+        </Link>
+      </div>
+      <img src={bg} alt="Profile" className="bg" />
+    </section>
+  );
+};
 
-export default Intro
-
+export default Intro;
