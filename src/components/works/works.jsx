c7159330dc07575038f76d2c169dbae9056e38e5
coin-123 <isobednlemchukwu@gmail.com>
2025-10-01 11:04:23 +0100

    refactor: update Works component with improved structure and accessibility; replace images with links

--- src/components/works/works.jsx
+++ src/components/works/works.jsx
@@ -1,33 +1,62 @@
-import React from 'react'
-import './works.css';
-import Logo7 from '../../asset/logo7.png';
-import Logo2 from '../../asset/logo2.png';
-import Logo3 from '../../asset/logo3.png';
-import Logo4 from '../../asset/logo4.png';
-import Logo5 from '../../asset/logo5.png';
-import Logo6 from '../../asset/logo6.png';
+import React from "react";
+import "./works.css";
+// import Logo7 from '../../asset/logo7.png';
+// import Logo2 from '../../asset/logo2.png';
+// import Logo3 from '../../asset/logo3.png';
+// import Logo4 from '../../asset/logo4.png';
+// import Logo5 from '../../asset/logo5.png';
+// import Logo6 from '../../asset/logo6.png';
 
 const Works = () => {
   return (
     <section id="works">
-      <h2 className="workstitle">
-        My Portfolio
-      </h2>
+      <h2 className="workstitle">My Portfolio</h2>
       <span className="workdes">
-        Explore a selection of my recent projects, showcasing my expertise in building responsive, user-centric web applications. Each project highlights my commitment to clean code, modern design, and seamless user experiences.
+        Explore a selection of my recent projects, showcasing my expertise in
+        building responsive, user-centric web applications. Each project
+        highlights my commitment to clean code, modern design, and seamless user
+        experiences.
       </span>
+
       <div className="workimgs">
-        <img src={Logo7} alt="" className="workimg" />
+        <a
+          href=""
+          target="_blank"
+          rel="noreferrer"
+          className="work-link"
+        >
+          https://obed-portfolio.vercel.app/
+        </a>
+
+        <a
+          href="https://streethub-academy-landingpage.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+          className="work-link"
+        >
+          https://streethub-academy-landingpage.vercel.app/
+        </a>
+
+        <a
+          href="https://e-cormmce.vercel.app/"
+          target="_blank"
+          rel="noreferrer"
+          className="work-link"
+        >
+          https://e-cormmce.vercel.app/
+        </a>
+
+        {/* <img src={Logo7} alt="" className="workimg" />
         <img src={Logo2} alt="" className="workimg" />
         <img src={Logo3} alt="" className="workimg" />
         <img src={Logo4} alt="" className="workimg" />
         <img src={Logo5} alt="" className="workimg" />
-        <img src={Logo6} alt="" className="workimg" />
+        <img src={Logo6} alt="" className="workimg" /> */}
       </div>
       {/* <button className="workbtn">
         See More
       </button> */}
     </section>
-  )
-}
-export default Works
+  );
+};
+export default Works;
